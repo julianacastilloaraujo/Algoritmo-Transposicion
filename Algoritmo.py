f47ec5238648cec1e54003a40894a8957c287b79
Juliana Castillo <jcastilloaraujo@icloud.com>
2023-09-20 09:01:09 -0500

    Algoritmo de matriz

---
+++ Algoritmo.py
@@ -0,0 +1,64 @@
+# Cifrado César
+
+TAM_MAX_CLAVE = ord('Z') - ord('A') + 1
+
+
+def obtenerModo():
+    while True:
+        print('¿Deseas encriptar o desencriptar un mensaje?')
+        modo = input().lower()
+        if modo in ('encriptar', 'e', 'desencriptar', 'd'):
+            return modo
+        else:
+            print('Ingresa "encriptar" o "e" o "desencriptar" o "d"')
+
+
+def obtenerMensaje():
+    print('Ingresa tu mensaje:')
+    return input()
+
+
+def obtenerClave():
+    clave = 0
+    while True:
+        print('Ingresa el número de clave (1-%s)' % (TAM_MAX_CLAVE))
+        clave = int(input())
+        if (clave >= 1 and clave <= TAM_MAX_CLAVE):
+            return clave
+
+
+def obtenerMensajeTraducido(modo, mensaje, clave):
+    if modo[0] == 'd':
+        clave = -clave
+    traduccion = ''
+
+    for simbolo in mensaje:
+        if simbolo.isalpha():
+            num = ord(simbolo)
+            num += clave
+
+            if simbolo.isupper():
+                if num > ord('Z'):
+                    num -= 26
+                elif num < ord('A'):
+                    num += 26
+            elif simbolo.islower():
+                if num > ord('z'):
+                    num -= 26
+                elif num < ord('a'):
+                    num += 26
+
+            traduccion += chr(num)
+        else:
+            traduccion += simbolo
+
+    return traduccion
+
+
+modo = obtenerModo()
+mensaje = obtenerMensaje()
+clave = obtenerClave()
+
+
+print('Tu texto traducido es:')
+print(obtenerMensajeTraducido(modo, mensaje, clave))
